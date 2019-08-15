2b885c8b8552fcee3389f9a2c19173d36a055cc6
dechuan <yangdechuan@pku.edu.cn>
2019-08-15 13:52:21 +0800

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+import io
+import json
+
+from snips_nlu import SnipsNLUEngine
+from snips_nlu.default_configs import CONFIG_EN
+
+# snips-nlu generate-dataset en ticket.yaml > ticket.json
+
+# The first step is to understand which intent the sentence is about.
+# The second step is to extract the parameters, a.k.a. the slots of the sentence.
+if __name__ == "__main__":
+    engine = SnipsNLUEngine(config=CONFIG_EN, random_state=233)
+
+    with io.open("dataset/ticket.json") as f:
+        dataset = json.load(f)
+    engine.fit(dataset)
+
+    parsing = engine.parse("Can I get 2 tickets for the big short?")
+    print(json.dumps(parsing, indent=2))
